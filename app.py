b5e2e697b6b9b0587e9c4c7f29f67c699c27321d
Harriswells68 <136472037+Harriswells68@users.noreply.github.com>
2024-10-02 11:42:51 +0000

    commit

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import time
 import requests
 import math
 
+idn=0
+skeys=['0646ee1ae11fddac4152a4d58aae97c6', 'd1f45e836c53f961c43126869c877332', '60c0c520be44a5752af2e25079cebd84', '7e851e8ef8ac3e2c875345105442bf54', 'cf2c98accb2e6dce7922c545d8fcd901']
+ids=['1e051fd3-9dc4-49bf-9007-432ef1dc1dfe', 'ba102dcc-a9d4-4328-85ab-bd624b097943', '607927a0-565d-4aa9-a25b-539ee8d8dc35', '08419b96-1f3f-4709-8ae7-74822a88b9dc', '22a45393-614b-436f-8361-8c9976bb43f2']
+
 coverpaget={
   "LineSpacing": "FullSize",
   "WrapMode": "ByWords",
@@ -12,10 +16,10 @@ coverpaget={
   "TopMargin": 0,
   "BottomMargin": 0,
   "Rectangle": {
-    "LLX": 60,
+    "LLX": 30,
     "LLY": 354,
-    "URX": 536,
-    "URY": 420
+    "URX": 566,
+    "URY": 554
   },
   "Rotation": 0,
   "SubsequentLinesIndent": 0,
@@ -27,7 +31,7 @@ coverpaget={
         {
           "Value": "My Physics Topic",
           "TextState": {
-            "FontSize": 34,
+            "FontSize": 25,
             "Font": "PlaywriteDEGrund-Regular",
             "ForegroundColor": {
               "A": 255,
@@ -59,7 +63,7 @@ coverpagen={
   "BottomMargin": 0,
   "Rectangle": {
     "LLX": 235,
-    "LLY": 220,
+    "LLY": 219,
     "URX": 560,
     "URY": 366
   },
@@ -73,8 +77,8 @@ coverpagen={
         {
           "Value": "SUJAL GARASIYA",
           "TextState": {
-            "FontSize": 20,
-            "Font": "LeagueSpartan-Black",
+            "FontSize": 21,
+            "Font": "LeagueSpartan-Bold",
             "ForegroundColor": {
               "A": 255,
               "R": 118,
@@ -87,8 +91,8 @@ coverpagen={
               "G": 0,
               "B": 0
             },
-            "FontStyle": "Regular",
-            "FontFile": "LeagueSpartan-Black.ttf"
+            "FontStyle": "Bold",
+            "FontFile": "LeagueSpartan-Bold.ttf"
           }
         }
       ]
@@ -105,10 +109,10 @@ certi={
   "TopMargin": 0,
   "BottomMargin": 0,
   "Rectangle": {
-    "LLX": 30,
+    "LLX": 15,
     "LLY": 357,
     "URX": 565,
-    "URY": 562
+    "URY": 555
   },
   "Rotation": 0,
   "SubsequentLinesIndent": 0,
@@ -151,7 +155,7 @@ ackn={
   "TopMargin": 0,
   "BottomMargin": 0,
   "Rectangle": {
-    "LLX": 380,
+    "LLX": 378,
     "LLY": 280,
     "URX": 540,
     "URY": 300
@@ -166,7 +170,7 @@ ackn={
         {
           "Value": "SUJAL GARASIYA",
           "TextState": {
-            "FontSize": 14,
+            "FontSize": 16,
             "Font": "SourceSerif4-Regular",
             "ForegroundColor": {
               "A": 255,
@@ -190,24 +194,17 @@ ackn={
 }
 datas=[coverpaget, coverpagen, certi, ackn]
 
+fail=False
+
 def designs(name, ptopic):
-    global topic, radio
-    if len(ptopic)>50:
-        l=topic.split()
-        m=math.ceil(len(l)/2)
-        t1=""
-        t2=""
-        for i in range(m):
-            t1=t1+l[i]+" "
-        for i in range(m, len(l)):
-            t2=t2+l[i]+" "
-        ptopic=t1+'\n\n'+t2
+    global topic, radio, idn, skeys, fail
+    fail=False
     pdfn=name.replace(" ", "")
     url = "https://api.aspose.cloud/connect/token"
     data = {
         "grant_type": "client_credentials",
-        "client_id": "1f47805e-0b4f-4b93-abe1-c30901a65320",
-        "client_secret": "7ca7be919737bce1cb80489209a0412d"
+        "client_id": ids[idn],
+        "client_secret": skeys[idn]
     }
     headers = {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -225,54 +222,61 @@ def designs(name, ptopic):
       with open(f'Frontpages.pdf', 'rb') as file:
         files={'file': file}
         response = requests.put(url, headers=headers, files=files, timeout=30)
+        if response.status_code==403:
+            idn=idn+1
+            designs(name, ptopic)
+            fail=True
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
          print(f"An error occurred: {e}")
     time.sleep(5)
-    for i in range(len(datas)):
-        pn=0
-        data=datas[i].copy()
-        if i in [0, 1]:
-            pn=1
-        else:
-            pn=i
-        if i in [0, 1, 3]:
-            if i==0:
-                data["Lines"][0]["Segments"][0]["Value"]=ptopic.upper()
-            elif i==1 or i==3:
+    if fail!=True:
+      for i in range(len(datas)):
+          pn=0
+          data=datas[i].copy()
+          if i in [0, 1]:
+              pn=1
+          else:
+              pn=i
+          if i in [0, 1, 3]:
+              if i==0:
+                  data["Lines"][0]["Segments"][0]["Value"]=ptopic.upper()
+              elif i==1:
                 data["Lines"][0]["Segments"][0]["Value"]=name.upper()
-        if i==2:
-            stra=checker(name, topic, radio)
-            data["Lines"][0]["Segments"][0]["Value"]=stra
-        try:
-            url = f"https://api.aspose.cloud/v3.0/pdf/{pdfn}.pdf/pages/{pn}/text"
-            headers = {
-              "Content-Type": "application/json",
-              "Authorization": f"Bearer {tk}",
-              "x-aspose-client": "Containerize.Swagger"
-            }
-            data = data
-            # Add a timeout to the request to handle potential network issues
-            response = requests.put(url, headers=headers, json=data, timeout=30)
-            response.raise_for_status()
-            print("Request successful!")
-        except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
-    time.sleep(3)
-    try:
-        url = f"https://api.aspose.cloud/v3.0/pdf/storage/file/{pdfn}.pdf"
-        headers = {
-          "Content-Type": "accept: multipart/form-data",
-          "Authorization": f"Bearer {tk}"  # Replace with your actual bearer token
-        }
-        # Add a timeout and stream the response to handle large files
-        response = requests.get(url, headers=headers, timeout=60)
-        time.sleep(2)
-        with open(f"pdfs/{pdfn}.pdf", "wb") as file:
-            file.write(response.content)
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-    download(f"{pdfn}.pdf")
+              elif i==3:
+                  data["Lines"][0]["Segments"][0]["Value"]=name.title()
+          if i==2:
+              stra=checker(name, topic, radio)
+              data["Lines"][0]["Segments"][0]["Value"]=stra
+          try:
+              url = f"https://api.aspose.cloud/v3.0/pdf/{pdfn}.pdf/pages/{pn}/text"
+              headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {tk}",
+                "x-aspose-client": "Containerize.Swagger"
+              }
+              data = data
+              # Add a timeout to the request to handle potential network issues
+              response = requests.put(url, headers=headers, json=data, timeout=30)
+              response.raise_for_status()
+              print("Request successful!")
+          except requests.exceptions.RequestException as e:
+              print(f"An error occurred: {e}")
+      time.sleep(3)
+      try:
+          url = f"https://api.aspose.cloud/v3.0/pdf/storage/file/{pdfn}.pdf"
+          headers = {
+            "Content-Type": "accept: multipart/form-data",
+            "Authorization": f"Bearer {tk}"  # Replace with your actual bearer token
+          }
+          # Add a timeout and stream the response to handle large files
+          response = requests.get(url, headers=headers, timeout=60)
+          time.sleep(2)
+          with open(f"pdfs/{pdfn}.pdf", "wb") as file:
+              file.write(response.content)
+      except requests.exceptions.RequestException as e:
+          print(f"An error occurred: {e}")
+      download(f"{pdfn}.pdf")
 
 def download(file):
     global b1, l1
@@ -312,6 +316,8 @@ b1=st.empty()
 c1=b1.button("Done")
 
 l1=st.empty()
+
+st.caption('If you got any errors please contact me:)')
 
 if c1:
     b1.empty()
